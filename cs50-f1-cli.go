8283ad872031dba8a5577a165693dca9ced9632d
Titus Moore <titusdmoore@gmail.com>
2024-03-18 16:56:12 -0400

    Added baseline functionality

--- cs50-f1-cli.go
+++ cs50-f1-cli.go
@@ -1,4 +1,4 @@
-package cs50f1cli
+package main
 
 import (
 	"fmt"
@@ -12,11 +12,51 @@ type model struct {
 }
 
 func main() {
-
+	p := tea.NewProgram(initialModel())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
+		os.Exit(1)
+	}
 }
 
 func initialModel() model {
 	return model{
 		active_page: 0,
 	}
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+
+		case "up", "k":
+			m.active_page++
+
+		case "down", "j":
+			m.active_page--
+
+		}
+	}
+
+	return m, nil
+}
+
+func (m model) View() string {
+	// The header
+	s := "Welcome to the Formula 1 CLI\n\n"
+
+	s += fmt.Sprintf("Active page: %d\n", m.active_page)
+
+	// The footer
+	s += "\nPress q to quit.\n"
+
+	// Send the UI for rendering
+	return s
 }
